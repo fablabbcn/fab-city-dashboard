fba9ade6970d9f79e417362b2acdbf786cc789e2
mqvlm <m.quintero09@gmail.com>
2016-10-03 17:39:52 +0200

    logo-navbar animation almost ok

--- app/static/js_custom/logoResize.js
+++ app/static/js_custom/logoResize.js
@@ -3,12 +3,14 @@
       if ($(document).scrollTop() == 0) {
         $('#logo-navbar').removeClass('logo-navbar-min');
         $('#logo-navbar').addClass('logo-max');
-        $('.logo-box img').attr('src', 'app/static/images/logos/logoFabCity.png');
+        $('.logo-box img').attr('src',  "{{ url_for('static', filename='images/logos/logoFabCity.png') }}");
+
+
       } else {
         $('#logo-navbar').removeClass('logo-max');
         $('#logo-navbar').addClass('logo-navbar-min');
-        $('.logo-box img').attr('src', 'app/static/images/logos/logo-min.png');
+        $('.logo-box img').attr('src',  "{{ url_for('static', filename='images/logos/logo-min.png') }}");
       }
     }); 
 
- 
+  
