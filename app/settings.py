6fb6f1d57f56f250af6a02f2f01368b11a5fb93f
rubenlorenzo <rubenlorenzo85@yahoo.es>
2017-03-30 13:55:15 +0200

    edit template reset_password and navigation admin

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,7 @@
 import os
 import config
 
- # This part below is based on the flask-user example under BSD license 
+ # This part below is based on the flask-user example under BSD license
 # Use a Class-based config to avoid needing a 2nd file
 # os.getenv() enables configuration through OS environment variables
 class ConfigClass(object):
@@ -19,11 +19,23 @@ class ConfigClass(object):
     MAIL_USE_SSL =        int(os.getenv('MAIL_USE_SSL',         True))
 
     # Flask-User settings
-    USER_APP_NAME        = "CITY_DASHBOARD"                # Used by email templates
+    USER_APP_NAME        = "FABCITY_DASHBOARD"                # Used by email templates
     USER_ENABLE_INVITATION = True
     USER_REQUIRE_INVITATION          = True
     USER_INVITE_URL= "/"
-    USER_AFTER_LOGIN_ENDPOINT                = "admin.home_page"
+
+    # Flask-User Endpoits
+    USER_AFTER_CHANGE_PASSWORD_ENDPOINT      = 'admin.edit_myprofile'              # v0.5.3 and up
+    USER_AFTER_CHANGE_USERNAME_ENDPOINT      = 'admin.edit_myprofile'              # v0.5.3 and up
+    USER_AFTER_CONFIRM_ENDPOINT              = 'admin.home_page'              # v0.5.3 and up
+    USER_AFTER_FORGOT_PASSWORD_ENDPOINT      = 'user.login'              # v0.5.3 and up
+    USER_AFTER_LOGIN_ENDPOINT                = 'admin.home_page'
+    USER_AFTER_LOGOUT_ENDPOINT               = 'user.login'    # v0.5.3 and up
+    USER_AFTER_REGISTER_ENDPOINT             = 'admin.home_page'              # v0.5.3 and up
+    USER_AFTER_RESEND_CONFIRM_EMAIL_ENDPOINT = 'user.login'              # v0.5.3 and up
+    USER_AFTER_RESET_PASSWORD_ENDPOINT       = 'admin.home_page'              # v0.6 and up
+    USER_INVITE_ENDPOINT = 'admin.home_page'
+
 
 # This part above is based on the flask-user example under BSD license
 
